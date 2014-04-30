58e1b0800f91d606690137bf8a4f3a7496644872
Bram Gotink <bram@gotink.me>
2014-04-30 21:51:06 +0200

    [content] first try at ingress profile

--- js/content.js
+++ js/content.js
@@ -241,7 +241,7 @@ window.iidentity = window.iidentity || {};
                                 return;
                             }
 
-                            console.error(err);
+                            modue.log.error(err);
                             return;
                         }
 
@@ -269,7 +269,7 @@ window.iidentity = window.iidentity || {};
                                 return;
                             }
 
-                            console.error(err);
+                            module.log.error(err);
                             return;
                         }
 
@@ -299,7 +299,7 @@ window.iidentity = window.iidentity || {};
                                 return;
                             }
 
-                            console.error(err);
+                            module.log.error(err);
                             return;
                         }
 
@@ -331,9 +331,163 @@ window.iidentity = window.iidentity || {};
             });
         },
 
+        createProfileWrapper = function () {
+            return $('<div>')
+                        .addClass('iidentity-profile-wrapper')
+                        .append(
+                            $('<div>')
+                                .addClass('iidentity-title-wrapper')
+                                .append(
+                                    $('<div>')
+                                        .addClass('F9a', 'iidentity-title')
+                                        .text('Ingress Agent')
+                                )
+                        )
+                        .append(
+                            $('<div>')
+                                .addClass('iidentity-profile')
+                        )
+                        .append(
+                            $('<div>')
+                        );
+        },
+        createProfile = function (player, wrapper) {
+            var $wrapper = $(wrapper),
+                $profile = $wrapper.find('> .iidentity-profile'),
+                tmp;
+
+            $profile.html('')
+                .append(
+                    $('<div>')
+                        .append(
+                            $('<div>')
+                                .addClass('iidentity-table')
+                                .append(
+                                    $('<div>')
+                                        .append(
+                                            $('<div>')
+                                                .addClass('iidentity-bold')
+                                                .text('Agent name')
+                                        )
+                                        .append(
+                                            $('<div>')
+                                                .text(player.nickname)
+                                        )
+                                )
+                                .append(
+                                    $('<div>')
+                                        .append(
+                                            $('<div>')
+                                                .addClass('iidentity-bold')
+                                                .text('Level')
+                                        )
+                                        .append(
+                                            $('<div>')
+                                                .text(player.level)
+                                        )
+                                )
+                                .append(
+                                    $('<div>')
+                                        .append(
+                                            $('<div>')
+                                                .addClass('iidentity-bold')
+                                                .text('Faction')
+                                        )
+                                        .append(
+                                            $('<div>')
+                                                .text(player.faction.substr(0, 1).toUpperCase() + player.faction.substr(1))
+                                        )
+                                )
+                        )
+                );
+
+            if ('anomaly' in player.extra) {
+                if (!Array.isArray(player.extra.anomaly)) {
+                    player.extra.anomaly = [ player.extra.anomaly ];
+                }
+
+                $profile.append(
+                    $('<div>')
+                        .append(
+                            $('<div>')
+                                .addClass('iidentity-subtitle')
+                                .text('Anomalies')
+                        )
+                        .append(
+                            $(player.extra.anomaly).map(
+                                function (anomaly) {
+                                    tmp = anomaly.substr(0, 1).toUpperCase() + anomaly.substr(1);
+
+                                    return $('<div>')
+                                        .append(
+                                            $('<img>')
+                                                .attr('src', chrome.extension.getURL('img/anomalies/' + anomaly + '.png'))
+                                                .attr('alt', anomaly)
+                                                .attr('title', tmp)
+                                                .addClass('iidentity-anomaly')
+                                        )
+                                        .append(
+                                            $('<span>')
+                                                .text(tmp)
+                                        );
+                                }
+                            )
+                        )
+                );
+            }
+        },
+        checkProfile = function () {
+            var $tabs = $('#contentPane div[role="tabpanel"]'),
+                oid,
+                $elem;
+
+            if ($tabs.length === 0) {
+                // not a profile!
+                return;
+            }
+
+            oid = $tabs.first().attr('id');
+            oid = oid.substr(0, oid.indexOf('-'));
+
+            if (oid.length !== 21) {
+                module.log.error('Invalid oid: %s', oid);
+                return;
+            }
+
+            doOnce($('#' + oid + '-about-page').last(), function ($root) {
+                module.log.log('Checking if player with oid %s exists', oid);
+                module.comm.getPlayer(oid, function (err, player) {
+                    if (err) {
+                        if (err === 'not-found') {
+                            module.log.log('No such player found');
+                            return;
+                        }
+
+                        module.log.error(err);
+                        return;
+                    }
+                    module.log.log('Player found: ', player);
+
+                    $elem = $root.find('div.iidentity-profile-wrapper');
+
+                    if ($elem.length === 0) {
+                        module.log.log('Creating profile wrapper');
+                        $elem = createProfileWrapper();
+                        $root.find('div.Ypa.jw.am').last().prepend($elem);
+                    } else {
+                        module.log.log('Re-using existing profile wrapper');
+                    }
+
+                    createProfile(player, $elem);
+                });
+            });
+        },
+
     // Start listening for new nodes to traverse
         observer = new window.MutationObserver(function (mutations) {
             var i;
+
+            checkProfile();
 
             mutations.forEach(function (mutation) {
                 for(i = 0; i < mutation.addedNodes.length; i++) {
@@ -345,12 +499,17 @@ window.iidentity = window.iidentity || {};
         forceUpdate = function () {
             doOnceTimestamp = '' + +new Date;
 
+            checkProfile();
             checkElement(window.document);
         };
 
-    forceUpdate();
+
     module.comm.setOnUpdate(forceUpdate);
 
-    observer.observe(window.document, { childList: true, subtree: true });
+    $(function () {
+        forceUpdate();
+
+        observer.observe(window.document, { childList: true, subtree: true });
+    });
 
 })(window.iidentity, window, window.jQuery);
