1ee17f4c80b634db3257a6361f0f2d017571060a
Bram Gotink <bram@gotink.me>
2014-05-19 20:45:50 +0200

    [backend] improved data merging code

--- js/data/merger.js
+++ js/data/merger.js
@@ -36,105 +36,7 @@ window.iidentity = window.iidentity || {};
             }
         },
 
-    // validation helpers
-
-        helpers = {
-            checkExists: function (obj, key, err) {
-                if (!Object.has(obj, key)) {
-                    err.push('Expected key "' + key + '" to exist.');
-                }
-            },
-            checkValidPage: function (obj, key, err) {
-                doEach(obj, key, function (value) {
-                    if (!Object.isString(value) || value.indexOf(':') === -1) {
-                        err.push('Invalid ' + key + ': "' + value + '"');
-                        return false;
-                    }
-                });
-            },
-            checkValidAnomaly: function (obj, key, err) {
-                doEach(obj, key, function (value) {
-                    if (!Object.isString(value) || anomalies.indexOf(
-                                value.compact().toLowerCase()
-                            ) === -1) {
-                        err.push('Invalid anomaly: "' + value + '"');
-                        return false;
-                    }
-                });
-            },
-            checkFactions: function (arr, err) {
-                if (arr.length === 0) {
-                    return;
-                }
-
-                var factions = {};
-
-                arr.exclude({ faction: 'unknown' }, { faction: 'error' }).each(function (object) {
-                    if (Object.has(object, 'faction')) {
-                        factions[
-                            ('' + object.faction).compact().toLowerCase()
-                        ] = true;
-                    }
-                });
-
-                if (Object.size(factions) > 1) {
-                    err.push('Player has multiple factions: ' + Object.keys(factions).join(', '));
-                    arr.each(function (object) {
-                        object.faction = 'error';
-                    });
-                }
-            },
-            checkValidLevel: function (object, err) {
-                if (!Object.has(object, 'level')) {
-                    return;
-                }
-
-                if (!(Object.isString(object.level) || Object.isNumber(object.level))
-                        && !('' + object.level).compact().match(/^([0-9]|1[0-6]|\?|)$/)) {
-                    err.push('Invalid level: "' + object.level + '"');
-                    delete object.level;
-                }
-            },
-            checkValidFaction: function (object, err) {
-                if (!Object.has(object, 'faction')) {
-                    return;
-                }
-
-                if (validFactions.indexOf(object.faction) === -1) {
-                    err.push('Invalid faction: "' + object.faction + '"');
-                    delete object.faction;
-                }
-            }
-        },
-
-    // pre-merge validation
-
-        pre_validate = function (arr, err) {
-            arr.each(function (object) {
-                if (Object.has(object, 'extra')) {
-                    helpers.checkValidPage(object.extra, 'event', err);
-                    helpers.checkValidPage(object.extra, 'community', err);
-
-                    helpers.checkValidAnomaly(object.extra, 'anomaly', err);
-                }
-
-                helpers.checkValidLevel(object, err);
-                helpers.checkValidFaction(object, err);
-            });
-
-            helpers.checkFactions(arr, err);
-        },
-
-    // post-merge validation
-
-        post_validate = function (object, err) {
-            helpers.checkExists(object, 'faction', err);
-            helpers.checkExists(object, 'level', err);
-            helpers.checkExists(object, 'nickname', err);
-            helpers.checkExists(object, 'oid', err);
-        },
-
-    // merge helpers
+    // validation & merging helpers
 
         getExtraDataValueName = function (str) {
             var i = str.indexOf(':');
@@ -168,6 +70,163 @@ window.iidentity = window.iidentity || {};
             });
         },
 
+        helpers = {
+            validate: {
+                checkExists: function (obj, key, err) {
+                    if (!Object.has(obj, key)) {
+                        err.push('Expected key "' + key + '" to exist.');
+                    }
+                },
+                checkValidPage: function (obj, key, err) {
+                    doEach(obj, key, function (value) {
+                        if (!Object.isString(value) || value.indexOf(':') === -1) {
+                            err.push('Invalid ' + key + ': "' + value + '"');
+                            return false;
+                        }
+                    });
+                },
+                checkValidAnomaly: function (obj, key, err) {
+                    doEach(obj, key, function (value) {
+                        if (!Object.isString(value) || anomalies.indexOf(
+                                    value.compact().toLowerCase()
+                                ) === -1) {
+                            err.push('Invalid anomaly: "' + value + '"');
+                            return false;
+                        }
+                    });
+                },
+                checkFactions: function (arr, err) {
+                    if (arr.length === 0) {
+                        return;
+                    }
+
+                    var factions = {};
+
+                    arr.exclude({ faction: 'unknown' }, { faction: 'error' }).each(function (object) {
+                        if (Object.has(object, 'faction')) {
+                            factions[
+                                ('' + object.faction).compact().toLowerCase()
+                            ] = true;
+                        }
+                    });
+
+                    if (Object.size(factions) > 1) {
+                        err.push('Player has multiple factions: ' + Object.keys(factions).join(', '));
+                        arr.each(function (object) {
+                            object.faction = 'error';
+                        });
+                    }
+                },
+                checkValidLevel: function (object, err) {
+                    if (!Object.has(object, 'level')) {
+                        return;
+                    }
+
+                    if (!(Object.isString(object.level) || Object.isNumber(object.level))
+                            && !('' + object.level).compact().match(/^([0-9]|1[0-6]|\?|)$/)) {
+                        err.push('Invalid level: "' + object.level + '"');
+                        delete object.level;
+                    }
+                },
+                checkValidFaction: function (object, err) {
+                    if (!Object.has(object, 'faction')) {
+                        return;
+                    }
+
+                    if (validFactions.indexOf(object.faction) === -1) {
+                        err.push('Invalid faction: "' + object.faction + '"');
+                        delete object.faction;
+                    }
+                },
+            },
+
+            merge: {
+                // default merge function
+                '.default': function (target, src, key) {
+                    target[key] = src[key];
+                },
+
+                // merge functions for specific data values
+                // for function func, src[func] is bound to exist,
+                // there's no guarantee for target[func] unless noted otherwise
+                err: function (target, src) {
+                    if (!Object.has(target, 'err')) {
+                        target.err = [];
+                    } else if (!Array.isArray(target, 'err')) {
+                        target.err = [ target.err ];
+                    }
+
+                    if (Array.isArray(src.err)) {
+                        target.err = target.err.concat(src.err);
+                    } else {
+                        target.err.push(src.err);
+                    }
+                },
+                faction: function (target, src) {
+                    if (!Object.has(target, 'faction')
+                            || (src.faction !== 'unknown' && target.faction !== 'error')) {
+                        target.faction = src.faction;
+                    }
+                },
+                extra: function (target, src) {
+                    // target has extra, see merge function
+                    var tmp;
+
+                    Object.each(src.extra, function (key, srcValue) {
+                        if (Object.has(target.extra, key)) {
+                            if (Array.isArray(target.extra[key])) {
+                                addToArray(
+                                    srcValue,
+                                    target.extra[key]
+                                );
+                            } else if (Object.isBoolean(target.extra[key])) {
+                                target.extra[key] = target.extra[key] || (!!srcValue);
+                            } else {
+                                tmp = [ target.extra[key] ];
+                                addToArray(
+                                    srcValue,
+                                    tmp
+                                );
+
+                                if (tmp.length > 1) {
+                                    target.extra[key] = tmp;
+                                }
+                            }
+                        } else {
+                            target.extra[key] = srcValue;
+                        }
+                    });
+                },
+            },
+        },
+
+    // pre-merge validation
+
+        pre_validate = function (arr, err) {
+            arr.each(function (object) {
+                if (Object.has(object, 'extra')) {
+                    helpers.validate.checkValidPage(object.extra, 'event', err);
+                    helpers.validate.checkValidPage(object.extra, 'community', err);
+
+                    helpers.validate.checkValidAnomaly(object.extra, 'anomaly', err);
+                }
+
+                helpers.validate.checkValidLevel(object, err);
+                helpers.validate.checkValidFaction(object, err);
+            });
+
+            helpers.validate.checkFactions(arr, err);
+        },
+
+    // post-merge validation
+
+        post_validate = function (object, err) {
+            helpers.validate.checkExists(object, 'faction', err);
+            helpers.validate.checkExists(object, 'level', err);
+            helpers.validate.checkExists(object, 'nickname', err);
+            helpers.validate.checkExists(object, 'oid', err);
+        },
+
         merge = function () {
             if (arguments.length === 0) {
                 return false;
@@ -177,62 +236,22 @@ window.iidentity = window.iidentity || {};
 
             var target = arguments[0],
                 src = arguments[1],
-                newArguments,
-                key,
-                extraKey,
-                tmp;
+                newArguments;
 
             if (!Object.isObject(target.extra)) {
                 target.extra = {};
             }
-
-            for (key in src) {
-                if (key === 'err') {
-                    if (Array.isArray(src.err)) {
-                        if (Object.has(target, 'err')) {
-                            if (Array.isArray(target.err)) {
-                                target.err = target.err.concat(src.err);
-                            } else {
-                                target.err = src.err;
-                            }
-                        } else {
-                            target.err = src.err;
-                        }
-                    }
-                } else if (key === 'extra') {
-                    for (extraKey in src.extra) {
-                        if (Object.has(target.extra, extraKey)) {
-                            if (Array.isArray(target.extra[extraKey])) {
-                                addToArray(
-                                    src.extra[extraKey],
-                                    target.extra[extraKey]
-                                );
-                            } else if (Object.isBoolean(target.extra[extraKey])) {
-                                target.extra[extraKey] = target.extra[extraKey] || src.extra[extraKey];
-                            } else {
-                                tmp = [ target.extra[extraKey] ];
-                                addToArray(
-                                    src.extra[extraKey],
-                                    tmp
-                                );
-
-                                if (tmp.length > 1) {
-                                    target.extra[extraKey] = tmp;
-                                }
-                            }
-                        } else {
-                            target.extra[extraKey] = src.extra[extraKey];
-                        }
-                    }
-                } else if (key === 'faction') {
-                    if (src.faction !== 'unknown'
-                            && !(Object.has(target, 'faction') && target.faction === 'error')) {
-                        target.faction = src.faction;
-                    }
-                } else {
-                    target[key] = src[key];
-                }
+            if (!Object.has(target, 'level')) {
+                target.level = 0;
             }
+
+            Object.keys(src).each(function (key) {
+                if (Object.has(helpers.merge, key)) {
+                    helpers.merge[key](target, src);
+                } else {
+                    helpers.merge['.default'](target, src, key);
+                }
+            });
 
             newArguments = Array.prototype.slice.call(arguments, 1);
             newArguments[0] = target;
